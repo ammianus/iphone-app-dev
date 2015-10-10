df4fb996810b182feee7e996e3a6fcc63fe7108e
Brian Laskey <brianl@brians-macbook-pro.local>
2015-10-10 19:24:03 -0400

    For iOS 8+ you need to request user permission for local notifications. Thanks to http://stackoverflow.com/questions/25948037/ios-8-uiapplication-sharedapplication-scheduledlocalnotifications-empty

--- BirthdayReminder/BRAppDelegate.m
+++ BirthdayReminder/BRAppDelegate.m
@@ -18,9 +18,11 @@
     
     //initialize all of our global custom appearence styles
     [BRStyleSheet initStyles];
+    //on iOS 8+ also need to registryUserNotificationSettings
+    [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert|UIUserNotificationTypeBadge|UIUserNotificationTypeSound categories:nil]];
     return YES;
 }
-							
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
